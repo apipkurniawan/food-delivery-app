cd5a316873f43cc911cd71f1eaaed2f2a3b0e030
Apip Kurniawan <davitaja92@gmail.com>
2023-05-08 10:22:55 +0700

    add dropdown search

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,36 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, StatusBar, TextInput, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import * as Icon from 'react-native-feather';
+import {themeColors} from '../theme/theme-color';
 
-function HomeScreen({navigation}: any) {
+function HomeScreen() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text className="bg-red-100">Home Screen</Text>
-      <Button
-        title="Go to Restaurant"
-        onPress={() => navigation.navigate('Restaurant')}
-      />
-    </View>
+    <SafeAreaView className="bg-white">
+      <StatusBar barStyle="light-content" />
+      {/* search bar */}
+      <View className="flex-row items-center space-x-2 mt-3 px-4 pb-2">
+        <View className="flex-row flex-1 items-center py-1 px-3 rounded-full border border-gray-300">
+          <Icon.Search height="25" width="25" stroke="gray" />
+          <TextInput placeholder="Restaurant" className="ml-2 flex-1" />
+          <View className="flex-row items-center space-x-1 border-0 pl-2 border-l-2 border-l-gray-300">
+            <Icon.MapPin height={20} width={20} stroke="gray" />
+            <Text className="text-gray-600">New York, NYC</Text>
+          </View>
+        </View>
+        <View
+          style={{backgroundColor: themeColors.bgColor(1)}}
+          className="p-3 rounded-full">
+          <Icon.Sliders
+            height={20}
+            width={20}
+            strokeWidth={2.5}
+            stroke="white"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 export default HomeScreen;
